6ace5acef9daa2e684dfdf89c0193727b507b731
kunalg12 <kunalgursal2923@gmail.com>
2025-01-25 23:38:11 +0530

    udpate create table cache table

--- backend/database/migrations/2025_01_25_180537_create_tasks_table.php
+++ backend/database/migrations/2025_01_25_180537_create_tasks_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description')->nullable();
-            $table->boolean('is_completed')->default(false);
             $table->timestamps();
         });
     }
